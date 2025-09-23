98cb512c98d59ebe72d5c754b16c8ffe1c4647ea
NicolasMor356 <morenopinzonn@gmail.com>
2025-09-22 22:59:21 -0500

    Update impares.py

--- Corte_1/AbordajeSecuencial/impares.py
+++ Corte_1/AbordajeSecuencial/impares.py
@@ -2,20 +2,20 @@ import os
 
 try:
     while True:
-        # pedir número al usuario
+        # pide el número al usuario
         num = input("Ingresa un número: ")
         
-        # limpiar consola
+        # limpia la consola
         os.system("cls" if os.name == "nt" else "clear")
         
         # convertir a entero
         try:
-            num = int(float(num))  # acepta enteros o decimales
+            num = int(float(num))  # solo acepta enteros o decimales
         except ValueError:
             print("Entrada inválida, escribe un número.")
             continue
         
-        # recorrer del 1 hasta num
+        # recorrer del 1 hasta la variable num
         for i in range(1, num+1):
             if i % 2 == 0:
                 print(f"{i} es par")

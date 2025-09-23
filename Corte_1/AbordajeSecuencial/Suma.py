e24173e41a4fc2f7e00c50fe472f9d03a5e6d4f1
NicolasMor356 <morenopinzonn@gmail.com>
2025-09-22 23:04:44 -0500

    Create Suma.py

---
+++ Corte_1/AbordajeSecuencial/Suma.py
@@ -0,0 +1,9 @@
+import math
+print("")
+radianes = float(input("ingrese valor en radianes: "))
+print("")
+proyeccion = math.sin(radianes)
+print("seno radianes = ", proyeccion, " ")
+print("radianes = ", radianes, " ")
+grados = math.degrees(radianes)
+print("grados = ", grados, " ")
